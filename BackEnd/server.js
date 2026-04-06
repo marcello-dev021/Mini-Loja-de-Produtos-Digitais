a94bbf3e206bf2044be626f144c9fb7efceb07f2
marcello-dev021 <marcelogithub036@gmail.com>
2026-04-06 10:19:19 -0300

    wda

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,19 @@
-import express from 'express'
-import pkg from "@prisma/client"
 
-const { PrismaClient } = pkg
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+
+
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
+
+
 const app = express()
+
 
 
 
